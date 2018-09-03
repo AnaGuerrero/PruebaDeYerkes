652167615e354929a1ee83550fc295a0989c0711
Ana Guerrero <anageorgina14@gmail.com>
2018-09-03 16:56:48 -0500

    Botones Virtuales Actualizados
    Se realizó un nuevo script en el que se junta la funcionalidad del botón añadir y del botón de mover. Sin embargo, el botón para mover, aún no funciona.

---
+++ Assets/VisionModality.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using System.ComponentModel;
+
+public class VisionModality : MonoBehaviour, IVirtualButtonEventHandler{
+
+    private GameObject btnAddCube;
+    private GameObject btnLeftCube;
+    List<GameObject> cubos = new List<GameObject>();
+    int numero = 0;
+
+    void Start(){
+        VirtualButtonBehaviour[] vbList = GetComponentsInChildren<VirtualButtonBehaviour>();
+        for (int i = 0; i < vbList.Length; ++i)
+        {
+            vbList[i].RegisterEventHandler(this);
+        }
+
+        btnAddCube = GameObject.Find("addButton");
+        btnLeftCube = GameObject.Find("btnLeft");
+
+        btnAddCube.SetActive(true);
+        btnLeftCube.SetActive(true);
+    }
+
+    void IVirtualButtonEventHandler.OnButtonPressed(VirtualButtonAbstractBehaviour vb){
+
+        Debug.Log("Botón presionado");
+
+        switch (vb.VirtualButtonName)
+        {
+            case "addButton":
+                Debug.Log("Entra a addButton");
+                /*btnAddCube.SetActive(false);
+                btnLeftCube.SetActive(true);*/
+                Object prefab = Resources.Load("CuboTextura");
+                GameObject cube = (GameObject)Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity);
+                numero = numero + 1;
+                TypeConverter converter = TypeDescriptor.GetConverter(typeof(int));
+                string sNumero = (string)converter.ConvertTo(numero, typeof(string));
+                cube.name = "Cubo" + sNumero;
+                cube.AddComponent<Lean.Touch.LeanSelectable>();
+                cube.AddComponent<Lean.Touch.LeanTranslate>();
+                cubos.Add(cube);
+                Debug.Log(cube.name);
+                break;
+            case "btnLeft":
+                Debug.Log("Botón Izquierdo");
+                /*btnAddCube.SetActive(true);
+                btnLeftCube.SetActive(false);*/
+                break;
+
+        }
+    }
+
+    void IVirtualButtonEventHandler.OnButtonReleased(VirtualButtonAbstractBehaviour vb)
+    {
+        
+    }
+}
