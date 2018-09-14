f5d7267320dea72a0aec7c6576eb893f8600be94
Ana Guerrero <anageorgina14@gmail.com>
2018-09-14 17:15:32 -0500

    Funcionalidad de Instrucciones, Imagen y salir.
    Se ha dado funcionalidad a los botones de instrucciones e imágen. Salir tiene funcionalidad en modo táctil. Se ha dado nuevo diseño a los botones virtuales, lo que ha provocado una falla en estos.

--- Assets/TouchModality.cs
+++ Assets/TouchModality.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.ComponentModel;
+using UnityEngine.UI;
+using System;
 
 public class TouchModality : MonoBehaviour {
 
@@ -11,12 +13,76 @@ public class TouchModality : MonoBehaviour {
     bool seleccionado;
     public Lean.Touch.LeanTranslate translate;
     int ultimo;
+    private string mMessage = "Mensaje de prueba!";
+    private bool mYesPressed = false;
+    public Texture texture;
+    private Image Background;
+    private Image image;
+    public Sprite sprite;
+    public Button cerrarFlyer;
+    public Button shape;
+    private Button closeInstr;
+    private Text txtButton;
+    private Text Instructions;
+    private Text txtInstr;
+
+    public void Start()
+    {
+        Screen.orientation = ScreenOrientation.Landscape;
+
+        System.Random random = new System.Random();
+        int aleatorio = random.Next(1, 4);
+        if(aleatorio == 1)
+        {
+            sprite = Resources.Load<Sprite>("sprite1");
+        }
+        if (aleatorio == 2)
+        {
+            sprite = Resources.Load<Sprite>("sprite2");
+        }
+        if (aleatorio == 3)
+        {
+            sprite = Resources.Load<Sprite>("sprite3");
+        }
+        if (aleatorio == 4)
+        {
+            sprite = Resources.Load<Sprite>("sprite4");
+        }
+        image = GameObject.Find("Image").GetComponent<Image>();
+        image.sprite = sprite;
+        image.enabled = true;
+
+        Background = GameObject.Find("Background").GetComponent<Image>();
+
+        cerrarFlyer = GameObject.Find("CerrarImagen").GetComponent<Button>();
+        txtButton = GameObject.Find("CerrarImagen").GetComponentInChildren<Text>();
+        txtButton.text = "Volver";
+        txtButton.alignment = TextAnchor.MiddleCenter;
+        txtButton.fontSize = 14;
+        cerrarFlyer.onClick.AddListener(ShapeOnClick);
+
+        shape = GameObject.Find("Shape").GetComponent<Button>();
+        shape.image.overrideSprite = sprite;
+
+        Instructions = GameObject.Find("txtInstructions").GetComponent<Text>();
+        Instructions.text = "Estas van a ser las instrucciones.";
+        Instructions.alignment = TextAnchor.MiddleCenter;
+        Instructions.fontSize = 14;
+
+        closeInstr = GameObject.Find("CloseInstructions").GetComponent<Button>();
+        txtInstr = GameObject.Find("CloseInstructions").GetComponentInChildren<Text>();
+        txtInstr.text = "Volver";
+        txtInstr.alignment = TextAnchor.MiddleCenter;
+        txtInstr.fontSize = 14;
+        closeInstr.onClick.AddListener(CloseInstructions);
+
+    }
 
     public void addComponent()
     {
         Debug.Log("Añadiendo componente");
 
-        Object prefab = Resources.Load("CuboTextura");
+        UnityEngine.Object prefab = Resources.Load("CuboTextura");
         GameObject cube = (GameObject)Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity);
         numero = numero + 1;
         TypeConverter converter = TypeDescriptor.GetConverter(typeof(int));
@@ -70,5 +136,103 @@ public class TouchModality : MonoBehaviour {
             cubos.Remove(cubos[ultimo - 1]);
             touchSelect();
         }
+    }
+
+    public void FinishTest()
+    {
+
+    }
+
+    public void ShowDialog()
+    {
+#if UNITY_EDITOR
+        Debug.Log("No compatible con esta plataforma");
+#endif
+#if UNITY_ANDROID
+        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+        activity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+        {
+            AndroidJavaObject alertDialogBuilder = new AndroidJavaObject("android/app/AlertDialog$Builder", activity);
+            alertDialogBuilder.Call<AndroidJavaObject>("setMessage", mMessage);
+            alertDialogBuilder.Call<AndroidJavaObject>("setPositiveButton", "De acuerdo", new PositiveButtonListener(this));
+            alertDialogBuilder.Call<AndroidJavaObject>("setCancelable", true);
+            AndroidJavaObject dialog = alertDialogBuilder.Call<AndroidJavaObject>("create");
+            dialog.Call("show");
+        }
+        ));
+#endif
+    }
+
+#if UNITY_ANDROID
+    private class PositiveButtonListener : AndroidJavaProxy
+    {
+        private TouchModality mDialog;
+        
+        public PositiveButtonListener(TouchModality tm): base("android.content.DialogInterface$OnClickListener")
+        {
+            mDialog = tm;
+        }
+
+        public void onClick(AndroidJavaObject obj, int value)
+        {
+            mDialog.mYesPressed = true;
+        }
+    }
+#endif
+
+    public void ShowImage()
+    {
+        var RectTransform = image.transform as RectTransform;
+        RectTransform.sizeDelta = new Vector2(300, 300);
+
+        var ButtonTransform = cerrarFlyer.transform as RectTransform;
+        ButtonTransform.sizeDelta = new Vector2(100, 50);
+
+        var BackTransform = Background.transform as RectTransform;
+        BackTransform.sizeDelta = new Vector2(Screen.width, Screen.height);
+    }
+    
+    public void ShapeOnClick()
+    {
+        var RectTransform = image.transform as RectTransform;
+        RectTransform.sizeDelta = new Vector2(0, 0);
+
+        var ButtonTransform = cerrarFlyer.transform as RectTransform;
+        ButtonTransform.sizeDelta = new Vector2(0, 0);
+
+        var BackTransform = Background.transform as RectTransform;
+        BackTransform.sizeDelta = new Vector2(0, 0);
+    }
+
+    public void GetInstructions()
+    {
+        var RectTransform = Instructions.transform as RectTransform;
+        RectTransform.sizeDelta = new Vector2(Screen.width, 300);
+
+        var BackTransform = Background.transform as RectTransform;
+        BackTransform.sizeDelta = new Vector2(Screen.width, Screen.height);
+
+        var ButtonTransform = closeInstr.transform as RectTransform;
+        ButtonTransform.sizeDelta = new Vector2(100, 50);
+    }
+
+    private void CloseInstructions()
+    {
+        var RectTransform = Instructions.transform as RectTransform;
+        RectTransform.sizeDelta = new Vector2(0, 0);
+
+        var BackTransform = Background.transform as RectTransform;
+        BackTransform.sizeDelta = new Vector2(0, 0);
+
+        var ButtonTransform = closeInstr.transform as RectTransform;
+        ButtonTransform.sizeDelta = new Vector2(0, 0);
+    }
+
+    public void Salir()
+    {
+        ScreenCapture.CaptureScreenshot("screenshot.jpg");
+        Application.Quit();
     }
 }
--- Assets/VisionModality.cs
+++ Assets/VisionModality.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using Vuforia;
 using System.ComponentModel;
+using UnityEngine.UI;
+using System;
 
 public class VisionModality : MonoBehaviour, IVirtualButtonEventHandler{
 
@@ -22,8 +24,16 @@ public class VisionModality : MonoBehaviour, IVirtualButtonEventHandler{
     int ultimo;
     bool seleccionado;
     int y = 0;
+    private UnityEngine.UI.Image Background;
+    private UnityEngine.UI.Image image;
+    private Text Instructions;
+    public Sprite sprite;
+
 
     void Start(){
+
+        Screen.orientation = ScreenOrientation.Landscape;
+
         VirtualButtonBehaviour[] vbList = GetComponentsInChildren<VirtualButtonBehaviour>();
         for (int i = 0; i < vbList.Length; ++i)
         {
@@ -53,6 +63,37 @@ public class VisionModality : MonoBehaviour, IVirtualButtonEventHandler{
         btnExit.SetActive(true);
         btnInstructions.SetActive(true);
         btnImage.SetActive(true);
+
+        System.Random random = new System.Random();
+        int aleatorio = random.Next(1, 4);
+
+        if (aleatorio == 1)
+        {
+            sprite = Resources.Load<Sprite>("sprite1");
+        }
+        if (aleatorio == 2)
+        {
+            sprite = Resources.Load<Sprite>("sprite2");
+        }
+        if (aleatorio == 3)
+        {
+            sprite = Resources.Load<Sprite>("sprite3");
+        }
+        if (aleatorio == 4)
+        {
+            sprite = Resources.Load<Sprite>("sprite4");
+        }
+
+        image = GameObject.Find("Image").GetComponent<UnityEngine.UI.Image>();
+        image.sprite = sprite;
+
+        Background = GameObject.Find("Background").GetComponent<UnityEngine.UI.Image>();
+
+        Instructions = GameObject.Find("txtInstructions").GetComponent<Text>();
+        Instructions.text = "Estas van a ser las instrucciones.";
+        Instructions.alignment = TextAnchor.MiddleCenter;
+        Instructions.fontSize = 14;
+
     }
 
     void IVirtualButtonEventHandler.OnButtonPressed(VirtualButtonAbstractBehaviour vb){
@@ -65,7 +106,7 @@ public class VisionModality : MonoBehaviour, IVirtualButtonEventHandler{
                 Debug.Log("Entra a addButton");
                 /*btnAddCube.SetActive(false);
                 btnLeftCube.SetActive(true);*/
-                Object prefab = Resources.Load("CuboTextura");
+                UnityEngine.Object prefab = Resources.Load("CuboTextura");
                 GameObject cube = (GameObject)Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity);
                 numero = numero + 1;
                 TypeConverter converter = TypeDescriptor.GetConverter(typeof(int));
@@ -130,8 +171,10 @@ public class VisionModality : MonoBehaviour, IVirtualButtonEventHandler{
             case "btnExit":
                 break;
             case "btnInstructions":
+                ShowInstructions();
                 break;
             case "btnImage":
+                ShowImage();
                 break;
 
         }
@@ -139,7 +182,15 @@ public class VisionModality : MonoBehaviour, IVirtualButtonEventHandler{
 
     void IVirtualButtonEventHandler.OnButtonReleased(VirtualButtonAbstractBehaviour vb)
     {
-        
+        switch (vb.VirtualButtonName)
+        {
+            case "btnInstructions":
+                HideInstructions();
+                break;
+            case "btnImage":
+                HideImage();
+                break;
+        }
     }
 
     public List<GameObject> GetList()
@@ -169,5 +220,41 @@ public class VisionModality : MonoBehaviour, IVirtualButtonEventHandler{
             cubos.Remove(cubos[ultimo - 1]);
             visionSelect();
         }
+    }
+
+    private void ShowImage()
+    {
+        var RectTransform = image.transform as RectTransform;
+        RectTransform.sizeDelta = new Vector2(Screen.height, Screen.height);
+
+        var BackTransform = Background.transform as RectTransform;
+        BackTransform.sizeDelta = new Vector2(Screen.width, Screen.height);
+    }
+
+    private void HideImage()
+    {
+        var RectTransform = image.transform as RectTransform;
+        RectTransform.sizeDelta = new Vector2(0, 0);
+
+        var BackTransform = Background.transform as RectTransform;
+        BackTransform.sizeDelta = new Vector2(0, 0);
+    }
+
+    private void ShowInstructions()
+    {
+        var BackTransform = Background.transform as RectTransform;
+        BackTransform.sizeDelta = new Vector2(Screen.width, Screen.height);
+
+        var RectTransform = Instructions.transform as RectTransform;
+        RectTransform.sizeDelta = new Vector2(Screen.width, Screen.height);
+    }
+
+    private void HideInstructions()
+    {
+        var RectTransform = Instructions.transform as RectTransform;
+        RectTransform.sizeDelta = new Vector2(0, 0);
+
+        var BackTransform = Background.transform as RectTransform;
+        BackTransform.sizeDelta = new Vector2(0, 0);
     }
 }
